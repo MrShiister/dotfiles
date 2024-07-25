663386eca00c30dfc1fb4d46e700f1c4ae6a3df7
MrShiister <31813485+MrShiister@users.noreply.github.com>
2024-07-25 05:30:32 +0000

    Change <leader><Space> to open float diagnostics
    - Remove codeAction (default <leader>cR)
    - Remove hover actions (default K)

--- dot_config/nvim/after/ftplugin/rust.lua
+++ dot_config/nvim/after/ftplugin/rust.lua
@@ -1,9 +1,9 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<leader><Space>", function()
-  vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-  -- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, { desc = "Rust: Code Action", silent = true, buffer = bufnr })
 
-vim.keymap.set("n", "gh", function()
-  vim.cmd.RustLsp("hover", "actions") -- supports rust-analyzer's grouping
-end, { desc = "Rust: Hover Actions", silent = true, buffer = bufnr })
+-- Open the floating error help
+vim.keymap.set(
+  "n",
+  "<leader><Space>",
+  vim.diagnostic.open_float,
+  { desc = "Diagnostic: Open Float", silent = true, buffer = bufnr }
+)
