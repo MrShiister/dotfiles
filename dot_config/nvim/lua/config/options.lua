747dc3accbe40e10027a84abd00f89cbcb117381
MrShiister <31813485+MrShiister@users.noreply.github.com>
2024-10-02 11:07:18 +0000

    Attempt clipboard fix

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,17 +1,41 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-if vim.fn.has("wsl") == 1 then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
   }
+end
+
+if vim.fn.has("wsl") == 1 then
+  if vim.env.REMOTE_CONTAINERS then
+    vim.g.clipboard = {
+      name = "WslInDevContainerClipboard",
+      -- run on your host (needs socat on both host and container):
+      -- socat tcp-listen:8121,fork,bind=0.0.0.0 EXEC:'clip.exe'
+      copy = {
+        ["+"] = "socat - tcp:172.17.0.1:8121",
+        ["*"] = "socat - tcp:172.17.0.1:8121",
+      },
+      paste = {
+        ["+"] = paste,
+        ["*"] = paste,
+      },
+      cache_enabled = 0,
+    }
+  else
+    vim.g.clipboard = {
+      name = "WslClipboard",
+      copy = {
+        ["+"] = "clip.exe",
+        ["*"] = "clip.exe",
+      },
+      paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      },
+      cache_enabled = 0,
+    }
+  end
 end
