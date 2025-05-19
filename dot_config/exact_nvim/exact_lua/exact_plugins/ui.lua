f55017f866d2fe1c7a02709744bec6b86cef9de1
ShiHong <31813485+MrShiister@users.noreply.github.com>
2025-05-19 13:38:11 +0800

    Switch from render-markdown to markview

--- dot_config/exact_nvim/exact_lua/exact_plugins/ui.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/ui.lua
@@ -98,16 +98,6 @@ return {
   },
 
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-
-  {
     "ibhagwan/fzf-lua",
     opts = function()
       local config = require("fzf-lua.config")
@@ -120,5 +110,16 @@ return {
       config.defaults.keymap.builtin["<c-f>"] = false
       config.defaults.keymap.builtin["<c-b>"] = false
     end,
+  },
+
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    -- dependencies = {
+    --     "saghen/blink.cmp"
+    -- },
   },
 }
