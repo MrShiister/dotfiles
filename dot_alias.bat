2d628c214fb12c65e81eb4d883ea2d0e8df7b33d
ShiHong <31813485+MrShiister@users.noreply.github.com>
2025-07-17 14:09:11 +0800

    Add vscmd doskey

--- dot_alias.bat
+++ dot_alias.bat
@@ -3,3 +3,4 @@
 
 DOSKEY nn=nvim -c "lua require(\"persistence\").load({last=true})"
 DOSKEY n=nvim .
+DOSKEY vscmd=cmd -- %comspec% /k "C:\Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/vcvars64.bat"
