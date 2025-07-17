f9f1c832f2442115cbc677ea884a41a9064f6b88
ShiHong <31813485+MrShiister@users.noreply.github.com>
2025-07-17 13:04:51 +0800

    Add .alias.reg

--- dot_alias.bat
+++ dot_alias.bat
@@ -1,6 +1,5 @@
 @echo off
-::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor\Autorun
-::string: full path to this path
+::Install .alias.reg to autorun this file in cmd
 
 DOSKEY nn=nvim -c "lua require(\"persistence\").load({last=true})"
 DOSKEY n=nvim .
